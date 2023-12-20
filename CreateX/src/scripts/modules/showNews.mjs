0a5e9f5724f0ce056e2f25dddc1a0a749722cb77
Oleksandr Hordiichuk <oleksandrhordiichuk@gmail.com>
2023-12-20 16:39:00 +0100

    Implemeted logic in recent news block

--- CreateX/src/scripts/modules/showNews.mjs
+++ CreateX/src/scripts/modules/showNews.mjs
@@ -2,7 +2,6 @@ import createNewsCard from '@js-templates/newsCard'
 
 const showNews = (container, data) => {
 	container.innerHTML = ''
-	console.log(container)
 	data.forEach(element => {
 		const card = createNewsCard(element)
 		container.appendChild(card)
